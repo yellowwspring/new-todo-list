8d6daa37315dfac900a24d28710ef5c8c887dee9
yellowwspring <yellowwspring@users.noreply.github.com>
2021-01-25 22:49:44 +0300

    Added test JavaScript code

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hlklk")
+const form = document.querySelector('form');
+form.addEventListener('submit', changeText);
+
+let counter = 0;
+
+function changeText(event) {
+    event.preventDefault();
+    let button = form.children[1];
+    button.innerText = counter;
+    counter++;
+}
+
